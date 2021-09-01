c2c156f6d24c2a64a810cbbeaf5813e94230c593
Vincent D. Warmerdam <vincentwarmerdam@gmail.com>
2021-09-01 11:26:49 +0200

    update

--- readme.py
+++ readme.py
@@ -1,12 +1,10 @@
-from rich import box
-from rich.columns import Columns
 from rich.console import Console
-from rich.panel import Panel
 from rich.tree import Tree
 
 console = Console(record=True, width=100)
 
 tree = Tree("🙂 [link=https://koaning.io]Vincent D. Warmerdam", guide_style="bold bright_black")
+
 python_tree = tree.add("📦 Open Source Packages", guide_style="bright_black")
 python_tree.add("[bold link=https://scikit-lego.netlify.app/]scikit-lego[/] - [bright_black]lego bricks for sklearn")
 python_tree.add("[bold link=https://koaning.github.io/human-learn/]human-learn[/] - [bright_black]rule-based components for sklearn")
@@ -17,19 +15,24 @@ python_tree.add("[bold link=https://koaning.github.io/memo/getting-started.html]
 python_tree.add("[bold link=https://calmcode.io/labs/drawdata.html]drawdata[/]    - [bright_black]draw datasets in jupyter")
 python_tree.add("[bold link=https://github.com/koaning/tokenwiser]tokenwiser[/]  - [bright_black]sklearn token tricks via .fit_partial()")
 python_tree.add("[bold link=https://github.com/koaning/mktestdocs]mktestdocs[/]  - [bright_black]turn markdown files into pytest tests")
+
 online_tree = tree.add("⭐ Online Projects", guide_style="bright_black")
 online_tree.add("[bold link=https://koaning.io]koaning.io[/]   - [bright_black]personal blog")
 online_tree.add("[bold link=https://calmcode.io]calmcode.io[/]  - [bright_black]dev education service")
 online_tree.add("[bold link=https://dearme.email]dearme.email[/] - [bright_black]reflection service")
+
 talk_tree = tree.add("🎙️ Popular Talks", guide_style="bright_black")
 talk_tree.add("[bold link=https://youtu.be/qcrR-Hd0LhI?t=542]Optimal Benchmarks and Other Failures[/]")
 talk_tree.add("[bold link=https://www.youtube.com/watch?v=nJAmN6gWdK8]Playing by the Rules-Based-Systems[/]")
 talk_tree.add("[bold link=https://www.youtube.com/watch?v=Z8MEFI7ZJlA]How to Constrain Artificial Stupidity[/]")
 talk_tree.add("[bold link=https://www.youtube.com/watch?v=kYMfE9u-lMo]The Profession of Solving the Wrong Problem[/]")
 talk_tree.add("[bold link=https://www.youtube.com/watch?v=68ABAU_V8qI]Winning with Simple, even Linear, Models[/]")
-employer_tree = tree.add("👨‍💻 Employer", guide_style="bright_black")
-employer_tree.add("[bold link=https://rasa.com]Rasa[/] - [bright_black]conversational software")
 
+employer_tree = tree.add("👨‍💻 Employer", guide_style="bright_black")
+rasa_tree = employer_tree.add("[bold link=https://rasa.com]Rasa[/] - [bright_black]conversational software")
+rasa_tree.add("[bold link=https://github.com/RasaHQ/rasa-nlu-examples]nlu examples[/] - [bright_black]custom nlu components")
+rasa_tree.add("[bold link=https://github.com/RasaHQ/taipo]taipo[/]        - [bright_black]data augmentation tools")
+rasa_tree.add("[bold link=https://github.com/RasaHQ/rasalit]rasalit[/]      - [bright_black]streamlit apps for rasa nlu")
 
 console.print(tree)
 console.print("")
